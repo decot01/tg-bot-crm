01d93ab50abcd47cee1f3cfd1fec9edd40a56b24
decot <109991592+decot01@users.noreply.github.com>
2023-06-23 17:26:35 +0300

    tg bot sql

--- botdevportal.py
+++ botdevportal.py
@@ -11,8 +11,9 @@ from pyowm.utils.config import get_default_config
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import ChatType
-
-#класс
+import sqlite3
+from aiogram import *
+import time
 class MyBot:
     #инициализация бота
     def __init__(self):
@@ -24,31 +25,66 @@ class MyBot:
     def start(self):
         #элементы клавиатуры
         help_button = KeyboardButton('/help 🎫')
-        price_button = KeyboardButton('/showdb ')
+        price_button = KeyboardButton('/showusersdb 📜')
         info_button = KeyboardButton('/statistics 📊')
-        pay_button = KeyboardButton('/sending ')
         keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(help_button, price_button, info_button, pay_button)
+        keyboard.add(help_button, price_button, info_button)
         #команды
         @self.dp.message_handler(commands=['start'])
         async def hello(message: types.message):
-            await message.answer('привет наши /help что бы узнать список команд')
+            await message.answer('привет напиши /help что бы узнать список команд')
 
         @self.dp.message_handler(commands=['help'])
         async def hello(message: types.message):
-            await message.answer('что умее этот бот???\n/price - цены и информация о занят  иях и группе\n/info - о нас \n/pay - оплата', reply_markup=keyboard)
+            await message.answer('🎫что умеет этот бот???🎫\n/showusersdb - покацавет информацию о зарегестрированных пользователях\n/statistics - количевство зарегестрированных позьлователей \n/sending - рассылка пользователям телеграмм канала', reply_markup=keyboard)
 
-        @self.dp.message_handler(commands=['/showdb'])
+        @self.dp.message_handler(commands=['showusersdb'])
         async def hello(message: types.message):
-            await message.answer('nothing...', reply_markup=keyboard)
+            # Создание подключения к базе данных
+            await message.answer('📜пожалуйста подождите идет обработка данных📜')
+            print("была показанна база данных время :",time.ctime())
+                # Создание подключения к базе данных
+            conn = sqlite3.connect('users.db')
+            cur = conn.cursor()
+            # Выполнение запроса к базе данных
+            cur.execute("SELECT * FROM users")
+            # Чтение результатов запроса
+            rows = cur.fetchall()
+            # Вывод результатов в консоль
+            for row in rows:
+                await message.answer(row)
+            print("была успешно выведены база данных время :",time.ctime())
+            # Закрытие подключения к базе данных
+            conn.close()
+            print('завершено соединения с базой данных время :',time.ctime())
 
         @self.dp.message_handler(commands=['statistics'])
         async def hello(message: types.message):
-            await message.answer('nothing...', reply_markup=keyboard)
+                # Создание подключения к базе данных
+            conn = sqlite3.connect('users.db')
+            cur = conn.cursor()
+
+            # Выполнение запроса к базе данных
+            cur.execute("SELECT numberid FROM users ORDER BY ROWID DESC LIMIT 1")
+
+            # Получение результата запроса
+            result = cur.fetchone()[0]
+
+            # Вывод результата в коноль
+            await message.answer(f"📊есть {result} пользователей записанных на ваше мероприятие📊")
+
+            # Закрытие подключения к базе данных
+            conn.close()
 
         @self.dp.message_handler(commands=['sending'])
         async def hello(message: types.message):
-            await message.answer('nothing...', reply_markup=keyboard)
+            mes = message.text
+            mes = mes.replace("/sending📨","")
+            mes = mes.replace("/sending","")
+            await message.answer('📨старт рассылки📨', reply_markup=keyboard)
+            await self.dp.bot.send_message(chat_id='1733251767', text=mes)#1001989620505    
+            print('начан старт рассылки по id 1001989620505 время :',time.ctime())
+            
 #запуск сервера
         executor.start_polling(self.dp, skip_updates=True)
 #старт сервера
--- botuserportal.py
+++ botuserportal.py
@@ -11,9 +11,39 @@ from pyowm.utils.config import get_default_config
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import ChatType
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Command
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types.reply_keyboard import ReplyKeyboardRemove
+from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher import FSMContext
+from aiogram import types
+import sqlite3
+import time 
+
+conn = sqlite3.connect('users.db')
+
+# Создаем таблицу для хранения информации о пользователях
+conn.execute('''CREATE TABLE IF NOT EXISTS users
+             (numberid INTEGER PRIMARY KEY AUTOINCREMENT,
+             username TEXT,
+             usersurname TEXT,
+             course TEXT,
+             userage TEXT,
+             pay TEXT,
+             contactnumbern TEXT
+             )''')
 
 #класс
-class MyBot:
+class MyBot(FSMContext,StatesGroup): 
+    # Создаем именованные состояния (states) для каждой стадии опроса
+    ENTER_NAME = State()
+    ENTER_SURNAME = State()
+    ENTER_COURSE = State()
+    ENTER_AGE = State()
+    ENTER_PAYMENT = State()
+    ENTER_CONTACT_NUMBER = State()
     #инициализация бота
     def __init__(self):
         self._TOKEN = '5689385764:AAFs8vZoZGrSYUN-kqkDifoj0I0lEZh1dnU'#токен
@@ -32,11 +62,11 @@ class MyBot:
         #команды
         @self.dp.message_handler(commands=['start'])
         async def hello(message: types.message):
-            await message.answer('привет наши /help что бы узнать список команд')
+            await message.answer('привет напиши /help что бы узнать список команд')
 
         @self.dp.message_handler(commands=['help'])
         async def hello(message: types.message):
-            await message.answer('что умее этот бот???\n/price - цены и информация о занят  иях и группе\n/info - о нас \n/pay - оплата', reply_markup=keyboard)
+            await message.answer('что умеет этот бот???\n/price - цены и информация о занят  иях и группе\n/info - о нас \n/pay - оплата \n если вы нашли ошибку просто очистие час с ботом и все!', reply_markup=keyboard)
 
         @self.dp.message_handler(commands=['price'])
         async def hello(message: types.message):
@@ -46,40 +76,87 @@ class MyBot:
         async def hello(message: types.message):
             await message.answer('nothing...', reply_markup=keyboard)
         #регистрация
-        @self.dp.message_handler(commands=['reg'])
-        async def registration_handler(msg: types.Message):
-            # Отправляем первый вопрос
-            await msg.answer("Как вас зовут?")
-            
-            # Ожидаем ответ
-            answer_1 = await bot.await_answer()
-            name = answer_1.text
-            
-            # Отправляем второй воп
-            await msg.answer("Сколько вам лет?")
-            
-            # Ожидаем ответ
-            answer_2 = await bot.await_answer()
-            age = answer_2.text
-            
-            #... добавляем другие вопросы ияем ответы
-            
-            # Выводим результат опроса
-            result_message = f"Спасибо за регистрацию, {name}! Ваш возраст {age}."
-            await msg.answer(result_message)
+    def register(self):
 
+        # Инициализируем клавиатуру
+        keyboard = ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button = KeyboardButton('нет ответа, перейти на следующий вопрос')
+        menu_button = KeyboardButton('/menu')
+        keyboard.add(button,menu_button)
+
+        # Обработчик команды "/reg"
+        @self.dp.message_handler(Command('reg'))
+        async def start_reg(message: types.Message):
+            await MyBot.ENTER_NAME.set()
+            await message.answer('Введите имя ребенка:', reply_markup=keyboard)
+
+        @self.dp.message_handler(state=MyBot.ENTER_NAME)
+        async def process_name(message: types.Message, state: FSMContext):
+            async with state.proxy() as data:
+                data['name'] = message.text
+            await MyBot.ENTER_SURNAME.set()
+            await message.answer('Введите фамилию ребенка:', reply_markup=keyboard)
+
+        @self.dp.message_handler(state=MyBot.ENTER_SURNAME)
+        async def process_surname(message: Message,state: FSMContext):
+            # Сохраняем фамилию ребенка и переходим на следующую стадию опроса
+            async with state.proxy() as data:
+                data['surname'] = message.text
+            await MyBot.ENTER_COURSE.set()
+            await message.answer('Введите курс или мероприятие на которое идет ребенок:', reply_markup=keyboard)
+
+        @self.dp.message_handler(state=MyBot.ENTER_COURSE)
+        async def process_course(message: Message, state: FSMContext):
+            # Сохраняем курс/мероприятие и переходим на следующую стадию опроса
+            async with state.proxy() as data:
+                data['course'] = message.text
+            await MyBot.ENTER_AGE.set()
+            await message.answer('Введите возраст ребенка:', reply_markup=keyboard)
+
+        @self.dp.message_handler(state=MyBot.ENTER_AGE)
+        async def process_age(message: Message, state: FSMContext):
+            # Сохраняем возраст ребенка и переходим на следующую стадию опроса
+            async with state.proxy() as data:
+                data['age'] = message.text
+            await MyBot.ENTER_PAYMENT.set()
+            await message.answer('Прошла ли у вас оплата ?', reply_markup=keyboard)
+
+        @self.dp.message_handler(state=MyBot.ENTER_PAYMENT)
+        async def process_payment(message: Message, state: FSMContext):
+            # Сохраняем ответ пользователя на вопрос об оплате и переходим на следующую стадию опроса
+            async with state.proxy() as data:
+                data['payment'] = message.text
+            await MyBot.ENTER_CONTACT_NUMBER.set()
+            await message.answer('Напишите ваш контактный номер начиная с +7:', reply_markup=keyboard)
+
+        @self.dp.message_handler(state=MyBot.ENTER_CONTACT_NUMBER)
+        async def process_contact_number(message: Message, state: FSMContext):
+            # Сохраняем контактный номер и выводим результат опроса
+            async with state.proxy() as data:
+                data['contact_number'] = message.text
+
+                username = data['name']
+                usersurname = data['surname']
+                course = data['course']
+                userage = data['age']
+                pay = data['payment']
+                contactnumber = data['contact_number']
+
+                await message.answer(f"Ваша анкета успешно принята!\nВот ваши данные:\nИмя ребенка: {username}\nФамилия ребенка: {usersurname}\nКурс/мероприятие: {course}\nВозраст ребенка: {userage}\nОплата: {pay}\nКонтактный номер: {contactnumber} чтобы заполнить 2 анкету очистите чат с ботом \n чтобы выыйти из заполнения анкеты пропишите /menu " ,reply_markup=keyboard)
+                conn.execute("INSERT INTO users (username, usersurname, course, userage, pay,contactnumber) VALUES (?, ?, ?, ?, ?, ?)",
+                (username, usersurname, course, userage, pay, contactnumber))
+                conn.commit()
+                # Закрываем соединение с базой данных
+                conn.close()                 
+                print("человек завершил регистрацию!время :",time.ctime())
+            await state.finish()
+        @self.dp.message_handler(commands=['menu'])
+        async def hello(message: types.message):
+            await message.answer('/price\n/info\n/pay\n если вы нашли ошибку просто очистие час с ботом и все!', reply_markup=keyboard)
 #запуск сервера
         executor.start_polling(self.dp, skip_updates=True)
 #старт сервера
 if __name__ == '__main__':
     bot = MyBot()
     bot.start()
-#examle
-'''
-        @self.dp.message_handler(commands=['reg'])
-        async def hello(msg: types.Message):
-                mes = msg.text
-
-                if mes == 'Привет':
-                    await self.dp.bot.send_message(chat_id=msg.from_user.id, text="Привет тебе тоже!")
-'''
+    bot.register()
